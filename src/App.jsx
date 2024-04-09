50e585a40c37014b16b54060c871e0ba7b892317
Arnar Johannsson <arnar.johannsson@qlok.se>
2024-04-09 10:03:11 +0200

    made our reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,44 @@
 import StateSolution from "./StateSolution";
+import { useState, useReducer } from "react";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
+    default:
+      throw new Error();
+  }
+};
 
 function App() {
-  return <StateSolution />;
+  const [userInput, setUserInput] = useState("");
+  const [count, setCount] = useState(0);
+  const [color, setColor] = useState(false);
+
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+
+  return (
+    <main className="App" style={{ color: color ? "#FFF" : "#FFF952" }}>
+      <input
+        type="text"
+        value={userInput}
+        onChange={(e) => setUserInput(e.target.value)}
+      />
+      <br />
+      <br />
+      <p>{state.count}</p>
+      <section>
+        <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+        <button onClick={() => dispatch({ type: "increment" })}>+</button>
+        <button onClick={() => setColor((prev) => !prev)}>Color</button>
+      </section>
+      <br />
+      <br />
+      <p>{userInput}</p>
+    </main>
+  );
 }
 
 export default App;
